7bb9e2bbb2ca2594c5b132cac129d2836f8bba7f
bistaprad <pradeep.bistapb@gmail.com>
2016-02-04 17:55:51 -0600

    reformatting, functions parameter added

--- crawler/dump_discussion_forum.py
+++ crawler/dump_discussion_forum.py
@@ -29,24 +29,34 @@ COLL_USER_SUMMARY = "user_summary"
 # for example, http://www.tudiabetes.org/forum/users/terry4/activity/replies
 COLL_USER_REPLY = "user_reply"
 
+# this collection saves specific user/discussion if it has been downloaded.
+# In case of interruption during the download this status helps to avoid the downloaded data again.
+# {collection_name = "discussions", id:"57213"}
+COLL_SAVE_STATUS = "status"
 
 def main(args):
     client = MongoClient(MONGODB_URI)
     db = client.tudiabetes
 
+    #***********************************
     # dump discussip topics and user list
-    # ft.dump_dicussion_toics(db, COLL_DISCUSSION_TOPICS) # dump discussion topics from the forum
-    # ft.dump_list_users(db, COLL_USERS) # dump all the users list
+    ft.dump_dicussion_toics(db, COLL_DISCUSSION_TOPICS) # dump discussion topics from the forum
+    print "*"*15 + "DISCUSSION LIST SAVED" + "*"*15
+    ft.dump_list_users(db, COLL_USERS) # dump all the users list
+    print "*"*15 + "USER LIST SAVED" + "*"*15
 
+    #***********************************
     # retrive discussion topic list from mongodb and dump complete discussion
     topic_ids = fm.get_topic_ids(db, COLL_DISCUSSION_TOPICS)  # retrieve list of topic ids and slug from mongo
-    ft.dump_discussion(topic_ids, db, COLL_DISCUSSION)  # dump all the discussion from forum **
+    ft.dump_discussion(topic_ids, db, COLL_DISCUSSION, COLL_SAVE_STATUS)  # dump all the discussion from forum
+    print "*"*15 + "DISCUSSIONS SAVED" + "*"*15
 
-    # retrieve user list from mongodb and dump all user ifo
+    # retrieve user list from mongodb and dump user information: summary and replies
     users = fm.get_users(db, COLL_USERS)  # retrieve list of usernames
-    ft.dump_user_summary(users, db, COLL_USER_SUMMARY)  # dump user summary for all users **
-    ft.dump_user_replies(users, db, COLL_USER_REPLY)  # dump user replies for all users *
-
+    ft.dump_user_summary(users, db, COLL_USER_SUMMARY, COLL_SAVE_STATUS)  # dump user summary for all users
+    print "*"*15 + "USER SUMMARY SAVED" + "*"*15
+    ft.dump_user_replies(users, db, COLL_USER_REPLY, COLL_SAVE_STATUS)  # dump user replies for all users
+    print "*"*15 + "DISCUSSION REPLLIES SAVED" + "*"*15
 
 if __name__ == '__main__':
     main(sys.argv[1:])
